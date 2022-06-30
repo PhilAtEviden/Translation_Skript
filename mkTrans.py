017c22ce08fa0e25d63ff5d0b93b08f137df960d
MrSteelTitan <philipp.buesgen23@gmail.com>
2022-06-30 09:53:17 +0200

    Added a init phase

--- mkTrans.py
+++ mkTrans.py
@@ -3,7 +3,7 @@ import pandas as pd
 Creates a excel file for every language with untranslated keys in the master CSV.
 """
 def make_translations():
-    master = pd.read_csv('CSV/master.csv', encoding='utf-8', index_col='key')
+    master = pd.read_csv('./CSV/master.csv', encoding='utf-8', index_col='key')
     export = master[master.isnull().any(axis=1)]
 
     for col in export.columns:
